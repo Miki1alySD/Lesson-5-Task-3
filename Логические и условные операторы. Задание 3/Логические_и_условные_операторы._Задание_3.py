a53dd431dedc14bb940a9d6bc12599fcfffa6b4c
79997 <79997@CYBERPC>
2023-05-11 14:20:07 +0400

    Добавьте файлы проекта.

---
+++ Логические и условные операторы. Задание 3/Логические_и_условные_операторы._Задание_3.py
@@ -0,0 +1,15 @@
+﻿summa=int(input("Минимальная сумма для инвестиций - "))
+Michel = int(input("Сумма для инвестиции на счету у Майкла - "))
+Ivan = int(input("Сумма для инвестиции на счету у Ивана - "))
+
+if (Michel>=summa) and (Ivan>=summa):
+    print(2)
+elif (Michel>=summa) and (Ivan<=summa):
+    print("Michel")
+elif (Michel<=summa) and (Ivan>=summa):
+    print("Ivan")
+
+elif (Michel<=summa) and (Ivan<=summa) and ((Michel+Ivan)>=summa):
+    print(1)
+elif (Michel<=summa) and (Ivan<=summa) and ((Michel+Ivan)<=summa):
+    print(0)
